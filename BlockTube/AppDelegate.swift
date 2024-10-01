d0738bc1ac27a72d3557f31f8a637e8af4c3d381
Ryan MacInnes <ryan.macinnes@gmail.com>
2024-10-01 14:40:18 +0200

    Initial Commit

---
+++ BlockTube/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BlockTube
+//
+//  Created by Ryan MacInnes on 10/1/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+}
---
+++ BlockTubeTests/BlockTubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlockTubeTests.swift
+//  BlockTubeTests
+//
+//  Created by Ryan MacInnes on 10/1/24.
+//
+
+import Testing
+@testable import BlockTube
+
+struct BlockTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
